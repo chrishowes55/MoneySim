6dbaefac4f9178dc1d8efcb84fadcabdd9c94fb1
chrishowes55 <chrishowes36@gmail.com>
2018-06-06 15:47:37 +0100

    Upgrades work-ish

--- MoneySim/src/com/liedssna/org/java/main/display/Display.java
+++ MoneySim/src/com/liedssna/org/java/main/display/Display.java
@@ -26,11 +26,11 @@ public class Display extends JFrame {
 
 	private Game game;
 	private Player player;
-	private JLabel label, noLabel;
-	private JPanel panel, normalPanel, upgradesPanel;
+	private JLabel label1, label2, noLabel;
+	private JPanel panel1, panel2, normalPanel, upgradesPanel;
 	private JButton upgradesButton, exitUpgradesButton;
-	private ArrayList<JLabel> labels = new ArrayList<JLabel>();
-	private ArrayList<JButton> buttons = new ArrayList<JButton>();
+	private ArrayList<JLabel> labels1 = new ArrayList<JLabel>(), labels2 = new ArrayList<JLabel>();
+	private ArrayList<JButton> buttons1 = new ArrayList<JButton>(), buttons2 = new ArrayList<JButton>();
 
 	public Display(String title, Player player, Game game) {
 		super(title);
@@ -51,32 +51,32 @@ public class Display extends JFrame {
 		//NORMAL PANEL
 		normalPanel = new JPanel();
 		normalPanel.setLayout(new GridLayout(0, 1));
-		label = new JLabel(player.getName() + " has £" + player.getMoney());
-		normalPanel.add(label);
+		label1 = new JLabel(player.getName() + " has £" + convertToNamed(player.getMoney()));
+		normalPanel.add(label1);
 		
 		noLabel = new JLabel("You cannot buy that");
 		noLabel.setVisible(false);
 		normalPanel.add(noLabel);
 
-		panel = new JPanel();
-		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
+		panel1 = new JPanel();
+		panel1.setLayout(new GridBagLayout());
+		GridBagConstraints c1 = new GridBagConstraints();
+		c1.fill = GridBagConstraints.HORIZONTAL;
 		for (int i = 0; i < game.getBusinesses().size(); i++) {
-			labels.add(new JLabel(game.getBusinesses().get((Integer)i).getName() + ": Time:" + game.getBusinesses().get((Integer)i).getTime() + ", Num:"
+			labels1.add(new JLabel(game.getBusinesses().get((Integer)i).getName() + ": Time:" + game.getBusinesses().get((Integer)i).getTime() + ", Num:"
 					+ game.getBusinesses().get((Integer)i).getNum()));
-			c.gridx = 0;
-			c.gridwidth = 2;
-			c.gridy = i + 1;
-			panel.add(labels.get(i), c);
-			c.gridx = 2;
-			c.gridy = i + 1;
-			JButton button = new JButton("Buy x1 for " + game.getBusinesses().get((Integer)i).getDisplayPrice());
-			button.addMouseListener(new ButtonMouseListener(i, game));
-			buttons.add(button);
-			panel.add(button, c);
+			c1.gridx = 0;
+			c1.gridwidth = 2;
+			c1.gridy = i + 1;
+			panel1.add(labels1.get(i), c1);
+			c1.gridx = 2;
+			c1.gridy = i + 1;
+			JButton button = new JButton("Buy x1 for " + convertToNamed(game.getBusinesses().get((Integer)i).getDisplayPrice()));
+			button.addMouseListener(new ButtonMouseListener(i, game, true));
+			buttons1.add(button);
+			panel1.add(button, c1);
 		}
-		normalPanel.add(panel);
+		normalPanel.add(panel1);
 		
 		upgradesButton = new JButton("Upgrades");
 		upgradesButton.addMouseListener(new UpgradesButtonMouseListener(this));
@@ -84,25 +84,61 @@ public class Display extends JFrame {
 		
 		//UPGRADES PANEL
 		upgradesPanel = new JPanel();
+		upgradesPanel.setLayout(new GridLayout(0, 1));
+		label2 = new JLabel(player.getName() + " has £" + convertToNamed(player.getMoney()));
 		upgradesPanel.add(new JLabel("UPGRADES"));
+		upgradesPanel.add(label2);
+		
+		panel2 = new JPanel();
+		panel2.setLayout(new GridBagLayout());
+		GridBagConstraints c2 = new GridBagConstraints();
+		c2.fill = GridBagConstraints.HORIZONTAL;
+		for (int i = 0; i < 5; i++) {
+			labels2.add(new JLabel(game.getUpgrades().get(i).getName()));
+			c2.gridx = 0;
+			c2.gridwidth = 2;
+			c2.gridy = i + 1;
+			panel2.add(labels2.get(i), c2);
+			c2.gridx = 2;
+			c2.gridy = i + 1;
+			JButton button = new JButton("Buy for " + convertToNamed(game.getUpgrades().get(i).getPrice()));
+			button.addMouseListener(new ButtonMouseListener(i, game, false));
+			buttons2.add(button);
+			panel2.add(button, c2);
+		}
+		upgradesPanel.add(panel2);
+		
 		exitUpgradesButton = new JButton("Back");
 		exitUpgradesButton.addMouseListener(new ExitUpgradesButtonMouseListener(this));
 		upgradesPanel.add(exitUpgradesButton);
 	}
 
 	public void updateDisplay() {
-		label.setText(player.getName() + " has £" + player.getMoney());
+		label1.setText(player.getName() + " has £" + player.getMoney());
+		label2.setText(player.getName() + " has £" + player.getMoney());
 	}
 	
 	public void updateTimes() {
-		for (int i = 0; i < labels.size(); i++) {
-			labels.get(i).setText(game.getBusinesses().get((Integer)i).getName() + ": Time:" + game.getBusinesses().get((Integer)i).getTime() + ", Num:"
+		for (int i = 0; i < labels1.size(); i++) {
+			labels1.get(i).setText(game.getBusinesses().get((Integer)i).getName() + ": Time:" + game.getBusinesses().get((Integer)i).getTime() + ", Num:"
 					+ game.getBusinesses().get((Integer)i).getNum());
 		}
 	}
 	
 	public void updatePrices(int i) {
-		buttons.get(i).setText("Buy x1 for " + game.getBusinesses().get((Integer)i).getDisplayPrice());
+		buttons1.get(i).setText("Buy x1 for " + game.getBusinesses().get((Integer)i).getDisplayPrice());
+	}
+	
+	public void updateUpgrades() {
+		for (int i = 0; i < 5; i++) {
+			if (i >= game.getUpgrades().size()) {
+				labels2.get(i).setText("No upgrades are left");
+				buttons2.get(i).setVisible(false);
+			} else {
+				labels2.get(i).setText(game.getUpgrades().get(i).getName());
+				buttons2.get(i).setText("Buy for " + game.getUpgrades().get(i).getPrice());
+			}
+		}
 	}
 	
 	public void noBuy() {
@@ -131,6 +167,19 @@ public class Display extends JFrame {
 		validate();
 		repaint();
 	}
+	
+	private int convertToNamed(int num) {
+		/* String newNum = "";
+		if (num / 1000000000 >= 1) {
+			newNum = (String) (num / 1000000000) + "Billion";
+		} else if (num / 1000000 >= 1){
+			newNum = (String) (num / 1000000) + "Million";
+		} else {
+			newNum = (String) num;
+		}
+		return newNum; */
+		return num;
+	} 
 
 	public class MyKeyListener implements KeyListener {
 
@@ -156,16 +205,22 @@ public class Display extends JFrame {
 	
 		private int index;
 		private Game game;
+		private boolean business;
 		
-		public ButtonMouseListener(int index, Game game) {
+		public ButtonMouseListener(int index, Game game, boolean business) {
 			this.index = index;
 			this.game = game;
+			this.business = business;
 		}
 		
 		
 		public void mousePressed(MouseEvent e) {
 			if (e.getButton() == MouseEvent.BUTTON1) {
-				game.increaseNumOfBusiness((Integer)index, 1);
+				if(business) {
+					game.increaseNumOfBusiness((Integer)index, 1);
+				} else {
+					game.buyUpgrade(index);
+				}
 			}
 		}
 		
